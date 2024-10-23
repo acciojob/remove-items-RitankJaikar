cc8b6e1a7ac8626d5467130f78e72bf70e599e01
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 14:44:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const select= document.querySelector("#colorSelect");
+const options= document.querySelectorAll("#colorSelect option");
+const button= document.querySelector("form input[type='button']");
+
+button.addEventListener("click", (e) => {
+	const selectValue= select.value;
+	//console.log(select.value);
+	options.forEach(option => {
+		if(option.value === selectValue) {
+			//console.log(option.value, selectValue);
+			option.remove();
+			return;
+		}
+	})
+})
+
+
+
+
+
